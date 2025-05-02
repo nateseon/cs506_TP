74ee64309021084027e2c5384b745c0bff56124e
MeghaN28 <n.megha82@gmail.com>
2025-05-02 12:04:25 -0700

    Data visualization and Comments

--- data_download.py
+++ data_download.py
@@ -1,5 +1,14 @@
-import kagglehub
-import pandas as pd
+'''
+Script:data_download.py
+Description:
+To download dataset, this script uses the kagglehub library to 
+It should be placed directly into your environment: Loan Approval Prediction from Kaggle.
+'''
 
+import kagglehub #Import the kagglehub library to programmatically download datasets from Kaggle
+import pandas as pd #Import pandas for general-purpose data manipulation (used later)
+
+#This will automatically handle caching and versioning of the dataset
+#Returns the local path where the dataset is saved
 path = kagglehub.dataset_download("architsharma01/loan-approval-prediction-dataset")
 print("Dataset path:", path)
--- data_load.py
+++ data_load.py
@@ -1,6 +1,14 @@
-import pandas as pd
+'''
+Script:data_load.py
+Description: This script loads the original raw loan approval dataset 
+and prints the first few rows for an initial look at the data structure and contents.
+'''
 
-csv_path = "/home/codespace/.cache/kagglehub/datasets/architsharma01/loan-approval-prediction-dataset/versions/1/loan_approval_dataset.csv"
+import pandas as pd #Import pandas for Reading and Handling Data
+
+# Step 1: Define the path to the original dataset CSV file
+csv_path = "loan_approval_dataset.csv"
+#Step 2: Read the dataset into a pandas DataFrame
 df = pd.read_csv(csv_path)
-
+#Step 3: Print the first 5 rows of the dataset to preview its structure
 print(df.head())
